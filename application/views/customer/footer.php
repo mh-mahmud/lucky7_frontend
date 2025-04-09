bafd91b9b62559930281150ca463ee996c3b2da2
Mh Tusher <mh.developer.me@gmail.com>
2025-04-09 22:42:37 +0600

    updtae footer panel

--- application/views/customer/footer.php
+++ application/views/customer/footer.php
@@ -800,15 +800,19 @@
 </script> 
 
 <!--Start of Tawk.to Script-->
-<script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/624bcb0c2abe5b455fc4c518/1gqgh19t6';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
-</script>
+
+	<!--
+	<script type="text/javascript">
+	var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+	(function(){
+	var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+	s1.async=true;
+	s1.src='https://embed.tawk.to/624bcb0c2abe5b455fc4c518/1gqgh19t6';
+	s1.charset='UTF-8';
+	s1.setAttribute('crossorigin','*');
+	s0.parentNode.insertBefore(s1,s0);
+	})();
+	</script>
+	-->
+
 <!--End of Tawk.to Script-->
