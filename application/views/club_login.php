4598cd895addb2737e818fc04731d94f0f46cd19
Anisur Rahman <anisur.rahman@mudozangl.com>
2025-04-10 12:50:07 +0600

    Repalce with lucky7uk

--- application/views/club_login.php
+++ application/views/club_login.php
@@ -83,7 +83,7 @@
 
 		<div class="form-header">
 			<img src="<?= base_url("assets/img/logo-new.png")?>" width="80" alt="">
-			<h3>Login To 5score Club</h3>
+			<h3>Login To Lucky7 UK Club</h3>
 			<p>Login to access your club dashboard</p>
 		</div>
 		<!--Email Input-->
